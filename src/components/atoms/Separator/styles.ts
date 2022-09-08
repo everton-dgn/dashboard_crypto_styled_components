d610113875031866c86245d63a6dd01ab8385c1d
Éverton Toffanetto <evertondgn@hotmail.com>
2022-09-08 14:24:07 -0300

    feat: add atoms components

--- src/components/atoms/Separator/styles.ts
+++ src/components/atoms/Separator/styles.ts
@@ -5,8 +5,8 @@ import T from 'theme'
 export const Content = styled.span<SeparatorProps>`
   ${({ maxWidth, maxHeight }) => css`
     background-color: ${T.colors.grey};
-    width: ${maxWidth ? '100%' : '0.1rem'};
-    height: ${maxHeight ? '100%' : '0.1rem'};
+    width: ${maxWidth ? '100%' : '1px'};
+    height: ${maxHeight ? '100%' : '1px'};
     max-width: ${maxWidth || 'inherit'};
     max-height: ${maxHeight || 'inherit'};
   `};

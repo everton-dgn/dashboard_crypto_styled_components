8731752b04e066729c10ede238a24f54a500a4df
Éverton Toffanetto <evertondgn@hotmail.com>
2022-09-08 19:48:22 -0300

    feat: add MainLayout in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,29 @@
 import { useSetPageTitle } from 'hooks'
 import * as S from './styles'
+import * as C from 'components'
+import nft from 'assets/images/nft.webp'
+import { DATA_CRYPTO } from './data'
 
 const Home = () => {
   useSetPageTitle({ pageTitle: 'Home' })
 
   return (
     <S.Container>
-      <h1>Hello World!</h1>
+      <S.MainLayout>
+        <C.MainBanner srcImage={nft} altDescription="NFT" />
+        <C.LastBuy dataLastBuy={DATA_CRYPTO} />
+        <S.WrapperCharts>
+          <C.StatisticsChart />
+          <C.AnalyticsChart dataAnalytics={DATA_CRYPTO} />
+        </S.WrapperCharts>
+        {DATA_CRYPTO.slice(0, 2).map(dataCrypto => (
+          <C.CryptocurrencyDataBalanceBox
+            key={dataCrypto.name}
+            cryptocurrencyDataBalance={dataCrypto}
+          />
+        ))}
+      </S.MainLayout>
+      <S.SecondaryLayout>SecondaryLayout</S.SecondaryLayout>
     </S.Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,6 +4,28 @@ import T from 'theme'
 export const Container = styled.main`
   display: grid;
   grid-template-columns: 1fr 35rem;
+  grid-auto-rows: max-content;
   column-gap: ${T.spacings.lg};
+  width: 100%;
+`
+
+export const MainLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: ${T.grid.gutterDesktop};
+  width: 100%;
+`
+
+export const WrapperCharts = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: ${T.grid.gutterDesktop};
+  height: 25.1rem;
+`
+
+export const SecondaryLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: ${T.grid.gutterDesktop};
   width: 100%;
 `

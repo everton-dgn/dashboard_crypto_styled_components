5057b107ceb488f1b7c0be80ad2a5d1c0d4d5eaa
Éverton Toffanetto <evertondgn@hotmail.com>
2022-09-10 21:03:30 -0300

    feat: change global styles

--- src/globalStyles/colors.ts
+++ src/globalStyles/colors.ts
@@ -10,6 +10,7 @@ export const ColorsTheme = css`
     --colors-focus: #12284c;
     --colors-grey-dynamic: #c9d2df;
     --colors-logo: #12284c;
+    --colors-chart: #00e0ff;
     --colors-gradient-overlay: radial-gradient(
       50% 50% at 50% 50%,
       #ebeef600 59%,
@@ -26,6 +27,7 @@ export const ColorsTheme = css`
     --colors-focus: #ffffff;
     --colors-grey-dynamic: #364763;
     --colors-logo: #ffffff;
+    --colors-chart: #ffffff;
     --colors-gradient-overlay: radial-gradient(
       50% 50% at 50% 50%,
       #00000000 59%,

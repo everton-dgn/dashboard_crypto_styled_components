4cf663381290b3cd0c85b067c2329fb89d36e217
Éverton Toffanetto <evertondgn@hotmail.com>
2022-09-16 01:55:39 -0300

    feat: add animations 2

--- src/components/organisms/VerticalMenu/styles.ts
+++ src/components/organisms/VerticalMenu/styles.ts
@@ -4,8 +4,7 @@ import T from 'theme'
 export const Container = styled.aside<{ isOpenMenu: boolean }>`
   display: grid;
   grid-template-columns: 100%;
-  width: 100%;
-  max-width: 28rem;
+  width: 28rem;
   border-radius: 0;
   box-shadow: 0 4px 15px -4px ${T.colors.shadow};
   background: ${T.colors.backgroundCard};

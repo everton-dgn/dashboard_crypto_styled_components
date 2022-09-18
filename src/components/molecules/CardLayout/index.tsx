8618aff512464b433903f61fc5bd3b868485bce5
Éverton Toffanetto <evertondgn@hotmail.com>
2022-09-17 21:47:15 -0300

    refactor: change span to h1

--- src/components/molecules/CardLayout/index.tsx
+++ src/components/molecules/CardLayout/index.tsx
@@ -7,7 +7,7 @@ const CardLayout = ({ title, optionsMenu, children }: CardLayoutProps) => (
   <S.Container>
     {title && (
       <S.Header>
-        <C.Typography text={title} type="text1" as="span" />
+        <C.Typography text={title} type="text1" as="h1" />
         {optionsMenu && (
           <C.IconButton icon={<IconOptionsMenu />} ariaLabel="Options menu" />
         )}

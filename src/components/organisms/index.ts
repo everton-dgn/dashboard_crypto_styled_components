0cdf133fa8c9ee787159ce5dd157eb36a94bc537
Éverton Toffanetto <evertondgn@hotmail.com>
2022-09-25 18:44:51 -0300

    perf: remove lazy components

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -4,9 +4,9 @@ export { default as AnalyticsChart } from './AnalyticsChart'
 export { default as CryptocurrencyDataBalanceBox } from './CryptocurrencyDataBalanceBox'
 export { default as LastBuy } from './LastBuy'
 export { default as MainBanner } from './MainBanner'
-export const QuickTransfer = lazy(() => import('./QuickTransfer'))
+export { default as QuickTransfer } from './QuickTransfer'
 export const SemesterChart = lazy(() => import('./SemesterChart'))
 export const StatisticsChart = lazy(() => import('./StatisticsChart'))
 export { default as UserProfile } from './UserProfile'
 export { default as VerticalMenu } from './VerticalMenu'
-export const Wallets = lazy(() => import('./Wallets'))
+export { default as Wallets } from './Wallets'

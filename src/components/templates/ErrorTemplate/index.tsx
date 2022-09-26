1a0dfb6e3ce435467989758e23f0a59286205c02
Éverton Toffanetto <evertondgn@hotmail.com>
2022-09-25 22:38:48 -0300

    fix: add 100vh

--- src/components/templates/ErrorTemplate/index.tsx
+++ src/components/templates/ErrorTemplate/index.tsx
@@ -2,9 +2,11 @@ import { Outlet } from 'react-router-dom'
 import * as S from './styles'
 
 const ErrorTemplate = () => (
-  <S.Grid>
-    <Outlet />
-  </S.Grid>
+  <S.CustomDiv100vh>
+    <S.Grid>
+      <Outlet />
+    </S.Grid>
+  </S.CustomDiv100vh>
 )
 
 export default ErrorTemplate
